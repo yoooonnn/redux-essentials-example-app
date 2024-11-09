3faf985c24cf0a7525f3fef2364ca11e3430e49e
yoooonnn <105402098+yoooonnn@users.noreply.github.com>
2024-11-10 01:41:01 +0900

    Main posts feed (#7)
    * #3
    
    * #4
    
    * #6

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
+import { Navbar } from './components/Navbar'
+import { AddPostForm } from './features/posts/AddPostForm'
+import { PostsList } from './features/posts/PostsList'
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <div className="App">
+        <Routes>
+          {/* 기본 경로("/")에 대해 PostsList를 렌더링 */}
+          <Route path="/" element={
+            <>
+              <AddPostForm />
+              <PostsList />
+            </>
+          } />
+
+          {/* 잘못된 경로로 접근할 경우 리디렉션 */}
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+      </div>
+    </Router>
+  )
+}
+
+export default App
