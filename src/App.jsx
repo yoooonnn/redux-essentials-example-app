a1948d357638d464a711a9f649356ede464d74bb
yoooonnn <yoooonnn@hanyang.ac.kr>
2024-11-10 01:37:45 +0900

    #6

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
 import { Navbar } from './components/Navbar'
+import { AddPostForm } from './features/posts/AddPostForm'
 import { PostsList } from './features/posts/PostsList'
 
 function App() {
@@ -10,7 +11,12 @@ function App() {
       <div className="App">
         <Routes>
           {/* 기본 경로("/")에 대해 PostsList를 렌더링 */}
-          <Route path="/" element={<PostsList />} />
+          <Route path="/" element={
+            <>
+              <AddPostForm />
+              <PostsList />
+            </>
+          } />
 
           {/* 잘못된 경로로 접근할 경우 리디렉션 */}
           <Route path="*" element={<Navigate to="/" />} />
