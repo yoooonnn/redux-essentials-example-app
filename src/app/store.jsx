c1543d3614e9c9589714f5761287d62825e1c6d9
yoooonnn <yoooonnn@hanyang.ac.kr>
2024-11-12 13:41:53 +0900

    Adding a UsersSlice #14

--- src/app/store.jsx
+++ src/app/store.jsx
@@ -2,11 +2,13 @@ import { configureStore } from '@reduxjs/toolkit'
 
 // import `postReducer` function
 import postsReducer from '../features/posts/postsSlice'
+import usersReducer from '../features/usres/usersSlice'
 
 // Update the call to `configureStore`
 // `postReducer` is being passed as a reducer field named `posts`
 export default configureStore({
     reducer: {
         posts: postsReducer,
+        users: usersReducer,
     }
 })
